f8d1c7b55ccbff5a043566b388976f353834b0b1
lilsaint9999 <61825146+lilsaint9999@users.noreply.github.com>
2020-11-14 14:00:25 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,10 +7,7 @@ public class Main {
         Animal animal = new Animal("Animal",1, 1,5,5);
         Dog dog = new Dog(":Yorkie", 8, 20, 2, 4,1,20,"silky");
         Fish fish = new Fish("pupi",1,1,2,2,2);
-//        dog.walk();
-//        dog.run();
-//
-//        dog.eat();
+
         animal.eat();
 
 
